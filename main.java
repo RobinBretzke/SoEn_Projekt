713f262f61bbc645ea847ca55dc7fe65a829e708
RobinBretzke <bretro01@thu.de>
2024-01-18 11:59:09 +0100

    Roboter fährt

--- main.java
+++ main.java
@@ -7,7 +7,7 @@ public class main {
     public static void main(String[] args) {
         RobotGUI robotGUI=new RobotGUI("Roboter GUI");
         robotGUI.setVisible(true);
-        Roboter Roboter=new Roboter(10,10,0,50,0, Color.red);
+        Roboter Roboter=new Roboter(100,100,0,50,0, Color.red);
         robotGUI.setRobot(Roboter);
         java.io.File file=new java.io.File("environment.txt");
         Environment env=EnvironmentLoader.loadFromFile(file);
