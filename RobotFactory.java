fef639427da691efa06cd1b70de3bb22d75211e8
liskst01 <liskst01@thu.de>
2024-01-18 14:09:12 +0100

    Klasse Sensor erstellt und in Robotfactory instanziiert-

--- RobotFactory.java
+++ RobotFactory.java
@@ -7,7 +7,7 @@ public class RobotFactory extends AbstractRobotFactory{
     @Override
     public Roboter createRobot() {
         Roboter Roboter=new Roboter(10,10,20,20,0, Color.red);
-        //Sensor
+        Sensor Sensor1 = new Sensor(0,Math.PI/4,10);
         return Roboter;
     }
 }
--- Sensor.java
+++ Sensor.java
@@ -1,5 +1,20 @@
 package thu.robots.components;
 
-public class Sensor {
-    //BaseSensor Sensor1 = new BaseSensor();
+import java.util.List;
+
+public class Sensor extends BaseSensor{
+    /**
+     * Erstellt einen neuen Sensor
+     *
+     * @param orientationToRobot Orientierung (Blickrichtung) des Sensors relativ zur Roboter-Ausrichtung
+     * @param beamWidth          Strahlbreite in rad
+     * @param measurementRate    Messrate in Hertz
+     */
+    public Sensor(double orientationToRobot, double beamWidth, int measurementRate) {
+        super(orientationToRobot, beamWidth, measurementRate);
+    }
+    @Override
+    public void measurementFromEnvironment(List<SensorData> data) {
+
+    }
 }
