5606e41059c7a9fb67b2bcdde9c3692527f2fc4c
bretro01 <bretro01@thu.de>
2024-01-25 15:54:21 +0100

    Automatische Steuerung verbessert 2.0

--- AutomatischeSteuerung.java
+++ AutomatischeSteuerung.java
@@ -5,24 +5,28 @@ import java.util.List;
 public class AutomatischeSteuerung {
     private Roboter roboter;
     private Environment environment;
+    private boolean hindernis;
 
     public AutomatischeSteuerung(Roboter roboter) {
         this.roboter = roboter;
     }
 
-    public SensorData lookForKontact(List<BaseSensor> sensors ) {
+    public SensorData findeKontakt(List<BaseSensor> sensors) {
         for (BaseSensor sensor : sensors) {
             List<SensorData> sensorDataList = Environment.simulateSensorData(roboter, sensor);
             for (SensorData sensorData : sensorDataList) {
                 if (sensorData.getDistance() < sensor.getMaxRange()) {
                     System.out.println("Hinderniss erkannt");
                     roboter.setVelocity(0);
+                    hindernis = true;
                     return sensorData;
                 }
 
             }
+            hindernis = false;
             return null;
         }
+        hindernis = false;
         return null;
     }
 
@@ -32,57 +36,57 @@ public class AutomatischeSteuerung {
 
             // Aktuelle Sensorwerte abrufen
             //environment.simulateSensorData(roboter);
-            //boolean Hindernis = false;
             List<BaseSensor> sensors = roboter.getSensors();
 
-            SensorData sensorData = lookForKontact(sensors);
-            double winkel=roboter.normalizeOrientation(Math.toDegrees(sensorData.getAngle()));
-                if (sensorData != null) {
-                    double newOrientation;
+            SensorData sensorData = findeKontakt(sensors);
+            double winkel = roboter.normalizeOrientation(Math.toDegrees(sensorData.getAngle()));
+            if (sensorData != null) {
+                double newOrientation;
 
-                    if (winkel <=0) {
-                        while (lookForKontact(sensors)!=null){
-                            newOrientation = roboter.getOrientation() - roboter.getOrientationIncrement();
-                            newOrientation=roboter.normalizeOrientation(newOrientation);
-                            roboter.setOrientation(newOrientation);
-                            try {
-                                Thread.sleep((long) (0.1 * 1000));
-                            } catch (InterruptedException ex) {
-                                break;
-                            }
-                        }
-                        newOrientation = roboter.getOrientation() + roboter.getOrientationIncrement();
-                        //System.out.println("Drehe nach unten");
-                    } else {
-                        newOrientation = roboter.getOrientation() - roboter.getOrientationIncrement();
-                        while (lookForKontact(sensors)!=null) {
-                            newOrientation = roboter.getOrientation() - roboter.getOrientationIncrement();
-                            newOrientation=roboter.normalizeOrientation(newOrientation);
-                            roboter.setOrientation(newOrientation);
-                            try {
-                                Thread.sleep((long) (0.1 * 1000));
-                            } catch (InterruptedException ex) {
-                                break;
-                            }
-                        }
-                        //System.out.println("Drehe nach oben");
-                    }
+                if (winkel <= 0) {
+
+                    newOrientation = roboter.getOrientation() - roboter.getOrientationIncrement();
+                    newOrientation = roboter.normalizeOrientation(newOrientation);
                     roboter.setOrientation(newOrientation);
+                    try {
+                        Thread.sleep((long) (0.1 * 1000));
+                    } catch (InterruptedException ex) {
+                        break;
+                    }
+
+
                 } else {
+                    newOrientation = roboter.getOrientation() - roboter.getOrientationIncrement();
 
-                    roboter.move(0.1);
+                    newOrientation = roboter.getOrientation() - roboter.getOrientationIncrement();
+                    newOrientation = roboter.normalizeOrientation(newOrientation);
+                    roboter.setOrientation(newOrientation);
+                    try {
+                        Thread.sleep((long) (0.1 * 1000));
+                    } catch (InterruptedException ex) {
+                        break;
+                    }
 
+                    //System.out.println("Drehe nach oben");
                 }
+            } else {
+
+                roboter.move(0.1);
+
+            }
 
             System.out.println(roboter.getPosX());
             System.out.println(roboter.getPosY());
-            roboter.setVelocity(20);
+            if (hindernis) {
+                roboter.setVelocity(0);
+            } else {
+                roboter.setVelocity(30);
+            }
             try {
                 Thread.sleep((long) (0.1 * 1000));
             } catch (InterruptedException ex) {
                 break;
             }
-            }
-
         }
     }
+}
--- main.java
+++ main.java
@@ -11,16 +11,19 @@ public class main {
      * @param args
      */
     public static void main(String[] args) {
-        java.io.File file=new java.io.File("TestEnvironment.txt");
+        java.io.File file=new java.io.File("environment.txt");
         Environment env=EnvironmentLoader.loadFromFile(file);
         RobotGUI robotGUI=new RobotGUI("Roboter GUI", env);
         robotGUI.setVisible(true);
         LinkedList<BaseSensor> Sensoren = new LinkedList<>();
         Sensor Sensor1 = new Sensor(0,2*Math.PI/3,10);
-        Sensor Sensor2 = new Sensor(0,Math.PI/4,5);
-        Sensoren.add(Sensor1);
+        /*Sensor Sensor2 = new Sensor(Math.PI/2,Math.PI/4,5);
+        Sensor Sensor3 = new Sensor(-Math.PI/2,Math.PI/4,5);
+        Sensoren.add(Sensor3);
         Sensoren.add(Sensor2);
-        Roboter Roboter= new Roboter(100,200,0,20,0, Color.red,Sensoren);
+        */
+        Sensoren.add(Sensor1);
+        Roboter Roboter= new Roboter(60,400,0,20,0, Color.red,Sensoren);
         robotGUI.setRobot(Roboter);
         robotGUI.setEnvironment(env);
         Roboter.activateAutonomousStearing();
